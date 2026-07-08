5f5f14fd93c63ca1d724b9a19cfaf7c43ecf067c
Arbitermartin <arbitermartin04@gmail.com>
2026-07-08 10:15:03 +0300

    add

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,13 +4,9 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
-  const stats = await utilities.getPortalStats()
-
-
   res.render("index", {
     title: "Youth Agro-services Network",
     nav,
-    stats,
     errors: null
   })
 }
