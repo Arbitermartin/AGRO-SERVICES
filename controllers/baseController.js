c4d2980201fc23102a16e8fe6ea45af6bfc6e940
Arbitermartin <arbitermartin04@gmail.com>
2026-07-10 11:35:30 +0300

    add

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,13 +2,36 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("pages/index", {
-    title: "Youth Agro-services Network",
-    nav,
-    errors: null
-  })
+// baseController.buildHome = async function(req, res){
+//   const nav = await utilities.getNav()
+//   res.render("pages/index", {
+//     title: "Youth Agro-services Network",
+//     nav,
+//     errors: null
+//   })
+// }
+baseController.buildHome = async function(req, res) {
+  try {
+    const latestNews = await accountModel.getLatestNews(5);
+    const upcomingEvents = await accountModel.getUpcomingEvents(5);
+
+    let nav = await utilities.getNav();
+
+    res.render("pages/index", {
+      title: "Home",
+      nav,
+      latestNews,
+      upcomingEvents,
+    });
+  } catch (err) {
+    console.error("Home page error:", err);
+    res.render("pages/index", {
+      title: "Home",
+      nav: await utilities.getNav(),
+      latestNews: [],
+      upcomingEvents: [],
+    });
+  }
 }
 
 module.exports=baseController;
