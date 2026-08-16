8b1aec16b16e89efb6ebb108dccea6cafa5b22d3
Martin Kasike <arbitermartin04@gmail.com>
2026-08-16 19:35:03 +0300

    add

--- database/knexfile.js
+++ database/knexfile.js
@@ -41,10 +41,13 @@ module.exports = {
 
     connection: {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+     port: Number(process.env.DB_PORT || 5432),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
+      ssl: {
+      rejectUnauthorized: false,
+    },
     },
 
     pool: {
